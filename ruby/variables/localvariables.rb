58d8019f597c0391718781f37a586d5c33974bb1
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-17 18:36:28 +0530

    Adds localvariables.rb in variables folder

---
+++ ruby/variables/localvariables.rb
@@ -0,0 +1,17 @@
+class Local
+	a=10
+	b=20
+		puts "multiplication of #{a} and #{b} is #{a*b}"	
+end	
+class Constant
+	V1=10
+	V2=15
+	def add
+		puts "Addition of #{V1} and #{V2} is #{V1+V2}"
+	end
+end
+
+obj2=Constant.new()
+obj2.add		
+	
+
