12eaec89b5102e0e4fe830af4df0795f590d70e0
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-17 18:34:28 +0530

    Adds operators folder in ruby

---
+++ ruby/operators/comparisonoperators.rb
@@ -0,0 +1,14 @@
+class Comparison
+	a=5
+	b=10
+	puts a==b
+	puts a!=b
+	puts a>b
+	puts a<b
+	puts a>=b
+	puts a<=b
+	puts a<=>b #Returns 0 if a=b,1 if a>b and -1 if a<b
+	puts 1==1.0
+    puts 1.eql?(1.0)
+end		
+
