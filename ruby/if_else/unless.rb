5bd1e74f9715fb3d1f393a37788b282e6ed5bbeb
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-18 11:01:48 +0530

    Adds unless.rb in if_else folder

---
+++ ruby/if_else/unless.rb
@@ -0,0 +1,7 @@
+#Execute if condition is false.If condition is true then else clause will be  executed.
+x=3
+unless x>5
+	puts "x is equal to or less than 5"
+else
+	puts "x is greater than 5"
+end		
