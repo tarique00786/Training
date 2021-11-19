dc72f9420ac68ae9973c038c67bf155a858fdf6b
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-19 11:57:11 +0530

    Adds until.rb in loops folder

---
+++ ruby/loops/until.rb
@@ -0,0 +1,11 @@
+class Until
+	factorial=1
+	i=1
+	print "Factorial of 5 is:  "
+	until i > 5     #Enter in loop until condition is false 
+		factorial *= i
+		i += 1
+	end
+	puts factorial
+end		 
+
