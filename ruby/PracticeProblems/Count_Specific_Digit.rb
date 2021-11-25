becd11ba2eae403b456a2a83a6914bbd7a22e0cc
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-25 17:53:27 +0530

    Adds Count_Specific_Digit.rb in PracticeProblems folder

---
+++ ruby/PracticeProblems/Count_Specific_Digit.rb
@@ -0,0 +1,13 @@
+class Countdigit
+  def specificdigit(n1,n2,n)
+    answer=Array.new
+  	for i in (n1..n2).to_a do
+      if i.to_s.include?(n.to_s) #convert every integer in string and checking if value is present or not
+        answer.push(n)  #append value at end
+      end   
+    end
+    p answer.size
+  end      
+end
+obj=Countdigit.new
+obj.specificdigit(100,200,1)
