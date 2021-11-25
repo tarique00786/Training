cb72eead3d0eadc7a01a50c888c2c7b885b4d610
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-25 17:58:25 +0530

    Edits Count_Specific_Digit.rb in PracticeProblems folder

--- ruby/PracticeProblems/Count_Specific_Digit.rb
+++ ruby/PracticeProblems/Count_Specific_Digit.rb
@@ -1,7 +1,7 @@
 class Countdigit
   def specificdigit(n1,n2,n)
     answer=Array.new
-  	for i in (n1..n2).to_a do
+    for i in (n1..n2).to_a do
       if i.to_s.include?(n.to_s) #convert every integer in string and checking if value is present or not
         answer.push(n)  #append value at end
       end   
