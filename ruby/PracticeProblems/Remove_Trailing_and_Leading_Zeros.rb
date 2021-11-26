597f9ab02ed992053a7b6051d76cb061bbe85e7c
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-26 14:52:00 +0530

    Edits Remove_Trailing_and_Leading_Zeros.rb

--- ruby/PracticeProblems/Remove_Trailing_and_Leading_Zeros.rb
+++ ruby/PracticeProblems/Remove_Trailing_and_Leading_Zeros.rb
@@ -1,3 +1,22 @@
+=begin
+Create a function that takes in a number as a string n and returns the number without trailing and leading zeros.
+
+Trailing Zeros are the zeros after a decimal point which don't affect the value 
+(e.g. the last three zeros in 3.4000 and 3.04000).
+
+Leading Zeros are the zeros before a whole number which don't affect the value 
+(e.g. the first three zeros in 000234 and 000230).
+
+Examples
+remove_leading_trailing("230.000") ➞ "230"
+
+remove_leading_trailing("00402") ➞ "402"
+
+remove_leading_trailing("03.1400") ➞ "3.14"
+
+remove_leading_trailing("30") ➞ "30"
+=end
+
 class RemoveZeroes
   def trailingzeros(str)
     value=str.to_f          #converts string in floating point number
