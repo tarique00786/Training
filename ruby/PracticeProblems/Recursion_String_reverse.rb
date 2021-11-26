ad02dcf6330600f624c32b60f7511a8d9466309b
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-26 12:48:27 +0530

    Edits Recursion_String_reverse.rb

--- ruby/PracticeProblems/Recursion_String_reverse.rb
+++ ruby/PracticeProblems/Recursion_String_reverse.rb
@@ -1,9 +1,9 @@
 class ReverseString
   def recursionreverse(str)
-    if str.empty?
+    if str.empty?         #Checks if string is empty , base case
       return ''
     else
-      str[-1]+recursionreverse(str.chop)
+      str[-1]+recursionreverse(str.chop)     #chop method will removes the last character 
     end
   end
 end
