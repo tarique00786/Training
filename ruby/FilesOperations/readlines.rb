8e404ec4d7f083d9908f4fa35e26a417afdb1ed9
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-30 14:07:04 +0530

    This program covers filelines operations

---
+++ ruby/FilesOperations/readlines.rb
@@ -0,0 +1,19 @@
+File.open("fileread.rb","r") do |file|
+  
+  #puts file.readline()  ->       Print the first line of file	
+   
+  #puts file.readline()  ->       Print the second line of the file 
+  
+  #puts file.readchar()  ->       Print the first character of file
+
+  puts file.readlines()[14]        #Prints the line on given index->14th line
+
+
+end  	
+=begin 
+#otput
+
+Examples
+
+
+=end
