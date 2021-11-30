19bc3950c8b0db43b69c5def0a3dc3bdd3b2138d
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-30 15:25:06 +0530

    Adds write method in comments

--- ruby/FilesOperations/filewrite.rb
+++ ruby/FilesOperations/filewrite.rb
@@ -2,3 +2,22 @@ File.open("test.txt","w") do |file|
   #Write method will overwrite the previous content of the file.	
   file.write("If you are enthusiastic about learning new things then you should join Beryl Systems")
 end  	
+
+=begin
+
+====Creation of a new file with content====== 
+
+File.open("filename.extension","w") do |file|
+  file.write(content)
+end 
+
+====append text at the last of the file======
+
+File.open("filename","a") do |file|
+  file.write(content)
+end  	
+
+
+
+
+
