e9225df5ff34635add605c72e2c1b6c7d82e1daa
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-30 13:49:18 +0530

    Adds a file fileread.rb in FileOperations folder

---
+++ ruby/FilesOperations/fileread.rb
@@ -0,0 +1,43 @@
+File.open("/home/aziz/Desktop/Training/ruby/PracticeProblems/longest_word.rb","r") do |file|
+	#Read file 
+	puts file.read()
+end	
+
+
+
+#output 
+
+=begin
+
+Longest Word
+Write a function that finds the longest word in a sentence. If two or more words are found, return the first longest word. Characters such as apostophe, comma, period (and the like) count as part of the word (e.g. O'Connor is 8 characters long).
+
+Examples
+longest_word("Hello darkness my old friend.") ➞ "darkness"
+
+longest_word("Hello there mate.") ➞ "Hello"
+
+longest_word("Margaret's toy is plastic.") ➞ "Margaret's"
+
+class Longest
+  def word(str)
+    answer=Array.new
+    arr=str.split(" ")
+    #return arr
+    max=arr[0].size
+    answer[0]=arr[0]
+    #return max
+    for i in 1...arr.size do
+      if arr[i].size>max
+        answer[0]=arr[i]
+        max=arr[i].size	
+      end
+    end  
+    return answer[0]
+  end
+end
+
+maxword=Longest.new
+p maxword.word("Margaret's toy is plastic.")  
+
+=end
