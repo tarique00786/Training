520c0c9890b1fbed25cb61ac9b77e05e4dfbb68a
tarique00786 <tariqueaziz00786@gmail.com>
2021-11-30 14:52:16 +0530

    This program overwrites the content of test.txt

---
+++ ruby/FilesOperations/filewrite.rb
@@ -0,0 +1,4 @@
+File.open("test.txt","w") do |file|
+  #Write method will overwrite the previous content of the file.	
+  file.write("If you are enthusiastic about learning new things then you should join Beryl Systems")
+end  	
