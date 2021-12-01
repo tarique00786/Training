777200d59c37a6498de0aceedeae021f528fbcad
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-01 17:10:17 +0530

    This program illustrate the use of include with class and module

---
+++ ruby/PracticeProblems/include.rb
@@ -0,0 +1,15 @@
+module SayHello
+  def sayhello
+    puts "Hello #{@name}"
+  end
+end
+
+class Person
+  include SayHello                              #Including the SayHello module
+  def initialize name
+    @name = name
+  end
+end
+
+person1=Person.new "Welcome to Beryl Systems"
+person1.sayhello                                #Calling the method of module with object
