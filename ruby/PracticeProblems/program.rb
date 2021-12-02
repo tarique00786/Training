fd9f445f0d6b222eb315d6fe5518380c14f32cd3
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-02 10:42:08 +0530

    This program takes the names from user and returns an array after adding Berylsystems in each name in postfix

---
+++ ruby/PracticeProblems/program.rb
@@ -0,0 +1,17 @@
+class Berylsystems
+  def printname(n)
+  	arr=Array.new(n)
+  	puts "enter name here"
+    for i in 0...n do 
+      name_=gets.chomp
+      arr[i]=name_
+    end
+    for i in 0...n do
+      arr[i] += "Berylsystems"
+    end  	
+    p arr
+  end
+end
+
+obj1=Berylsystems.new
+obj1.printname(5)   	
