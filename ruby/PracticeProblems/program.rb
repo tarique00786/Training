9e5396325cd52c6ffe1728b44fcead8c9ebaaf60
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-02 12:05:24 +0530

    Edits and formatted the code

--- ruby/PracticeProblems/program.rb
+++ ruby/PracticeProblems/program.rb
@@ -1,17 +1,17 @@
 class Berylsystems
   def printname(n)
-  	arr=Array.new(n)
-  	puts "enter name here"
-    for i in 0...n do 
-      name_=gets.chomp
-      arr[i]=name_
+    arr = Array.new(n)
+        puts "enter name here"
+        for i in 0...n do 
+            name = gets.chomp
+            arr[i] = name
+        end
+        for i in 0...n do
+            arr[i] += "Berylsystems"
+        end   
+        p arr
     end
-    for i in 0...n do
-      arr[i] += "Berylsystems"
-    end  	
-    p arr
-  end
 end
 
-obj1=Berylsystems.new
-obj1.printname(5)   	
+obj1 = Berylsystems.new
+obj1.printname(5)     
