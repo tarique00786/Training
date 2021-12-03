17591a67a938c1bce1d06bcac462461f0c058e8e
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-03 07:49:48 +0530

    Applied simple match route, root route and Default rout

--- rails/my_first_project/config/routes.rb
+++ rails/my_first_project/config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
-  get 'demo/index'
+  #root 'demo#index'
+
+  #get 'demo/index'
+
+  get ':controller(/:action(/:id))' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
