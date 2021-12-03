acf1f3c827cf64379f95746bd8e0969ce2f906d0
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-03 18:10:31 +0530

    Adds demo/contact and demo/about routs in demo controller

--- rails/my_first_project/app/controllers/demo_controller.rb
+++ rails/my_first_project/app/controllers/demo_controller.rb
@@ -1,4 +1,5 @@
 class DemoController < ApplicationController
+
   def index
     #render('index') 
     @id = params['id']
@@ -9,7 +10,21 @@ class DemoController < ApplicationController
     @array = [25,26,23,24,28]
     #render('hello')
     #redirect_to(:controller => 'demo', :action => 'index')
-          
   end
-      
+
+  def contact
+    if ['in','nz'].include?(params[:country])
+      @phone = '(+91) 223322332233'
+    elsif params[:country] == 'uk'
+      @phone = '(+10) 2222222222'
+    else
+      @phone = '(+21) 1111111111'
+    end        
+    render('contact_us')
+  end
+
+  def about
+    render('about_us')    
+  end
+
 end
