dcf59a7b65cd2dff473551fa7491c1d65e87e3ab
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-03 18:25:22 +0530

    Adds routs, files for assignment given in tutorial

--- rails/my_first_project/app/controllers/demo_controller.rb
+++ rails/my_first_project/app/controllers/demo_controller.rb
@@ -1,5 +1,4 @@
 class DemoController < ApplicationController
-
   def index
     #render('index') 
     @id = params['id']
@@ -10,21 +9,7 @@ class DemoController < ApplicationController
     @array = [25,26,23,24,28]
     #render('hello')
     #redirect_to(:controller => 'demo', :action => 'index')
+          
   end
-
-  def contact
-    if ['in','nz'].include?(params[:country])
-      @phone = '(+91) 223322332233'
-    elsif params[:country] == 'uk'
-      @phone = '(+10) 2222222222'
-    else
-      @phone = '(+21) 1111111111'
-    end        
-    render('contact_us')
-  end
-
-  def about
-    render('about_us')    
-  end
-
+      
 end
--- rails/my_first_project/config/routes.rb
+++ rails/my_first_project/config/routes.rb
@@ -3,6 +3,8 @@ Rails.application.routes.draw do
 
   get 'demo/index'
   get 'demo/hello'
+  get 'demo/contact'
+  get 'demo/about'
 
   #get ':controller(/:action(/:id))' 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
