ac4bd7c36c693bb5ffe3ef0f52fff44eceb28aaf
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-03 16:33:28 +0530

    This commit illustrates how to work with url paramaters

--- rails/my_first_project/app/controllers/demo_controller.rb
+++ rails/my_first_project/app/controllers/demo_controller.rb
@@ -1,12 +1,15 @@
 class DemoController < ApplicationController
   def index
-    render('index')  
+    #render('index') 
+    @id = params['id']
+    @page = params[:page] 
   end
 
   def hello
     @array = [25,26,23,24,28]
     #render('hello')
-    #redirect_to(:controller => 'demo', :action => 'index')    
+    #redirect_to(:controller => 'demo', :action => 'index')
+          
   end
       
 end
