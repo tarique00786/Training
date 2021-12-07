c48adbdaa9f1f413c003a1830b925ec654dba279
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-07 14:41:02 +0530

    Changes happens regarding droping join migration

--- rails/my_first_project/db/schema.rb
+++ rails/my_first_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_07_085141) do
+ActiveRecord::Schema.define(version: 2021_12_07_090056) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,11 +25,6 @@ ActiveRecord::Schema.define(version: 2021_12_07_085141) do
     t.datetime "updated_at", precision: 6, null: false
     t.bigint "subject_id", null: false
     t.index ["subject_id"], name: "index_pages_on_subject_id"
-  end
-
-  create_table "pages_subjects", id: false, force: :cascade do |t|
-    t.bigint "subject_id", null: false
-    t.bigint "page_id", null: false
   end
 
   create_table "subjects", force: :cascade do |t|
