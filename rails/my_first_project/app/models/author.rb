fc7995ce1542f521f6e860b3c6d0f0aa8e2d6cd7
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-15 17:47:56 +0530

    Did changes in models to use methods of query interface

--- rails/my_first_project/app/models/author.rb
+++ rails/my_first_project/app/models/author.rb
@@ -2,4 +2,7 @@ class Author < ApplicationRecord
 
   has_many :books, 	-> { order(published_at: :desc) }
   has_many :reviews
+  def authorname
+    "I am #{name}"
+  end  
 end
--- rails/my_first_project/app/models/book.rb
+++ rails/my_first_project/app/models/book.rb
@@ -2,9 +2,9 @@ class Book < ApplicationRecord
   
   belongs_to :author
   has_many :reviews
-  scope :test1, -> { where(author_id: 1)}	
-  scope :test1_expensive, -> { test1.where("price > 500")}
-  scope :cost_more_than, ->(amount) {where("price > ?", amount)}
-  scope :created_before, ->(time) {where("created_at < ?", time) if time.present?}
+  #scope :test1, -> { where(author_id: 1)}	
+  #scope :test1_expensive, -> { test1.where("price > 500")}
+  #scope :cost_more_than, ->(amount) {where("price > ?", amount)}
+  #scope :created_before, ->(time) {where("created_at < ?", time) if time.present?}
   #default_scope {where(author_id: 1)}
 end
