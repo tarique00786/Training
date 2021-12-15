346326a5f2afe9d4f4d4f8c48388a27d19603516
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-15 15:34:53 +0530

    Changes in table for using scope

--- rails/my_first_project/app/models/book.rb
+++ rails/my_first_project/app/models/book.rb
@@ -1,5 +1,7 @@
 class Book < ApplicationRecord
   
   belongs_to :author
-  has_many :reviews	
+  has_many :reviews
+  scope :test1, -> { where(author_id: 1)}	
+  scope :test1_expensive, -> { test1.where("price > 500")}
 end
--- rails/my_first_project/db/schema.rb
+++ rails/my_first_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_065508) do
+ActiveRecord::Schema.define(version: 2021_12_15_093839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,7 @@ ActiveRecord::Schema.define(version: 2021_12_15_065508) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "name"
+    t.decimal "price"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
