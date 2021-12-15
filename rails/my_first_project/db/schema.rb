1398c84de0f257a8f92f99c72e34d0d8b60ebdf8
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-15 12:16:56 +0530

    Added foreign key in book

--- rails/my_first_project/db/schema.rb
+++ rails/my_first_project/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_15_061708) do
+ActiveRecord::Schema.define(version: 2021_12_15_063301) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -131,6 +131,8 @@ ActiveRecord::Schema.define(version: 2021_12_15_061708) do
     t.integer "rating"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "book_id", null: false
+    t.index ["book_id"], name: "index_reviews_on_book_id"
   end
 
   create_table "subjects", force: :cascade do |t|
@@ -162,4 +164,5 @@ ActiveRecord::Schema.define(version: 2021_12_15_061708) do
   add_foreign_key "assemblies_parts", "assemblies"
   add_foreign_key "assemblies_parts", "parts"
   add_foreign_key "employees", "employees", column: "manager_id"
+  add_foreign_key "reviews", "books"
 end
