a9fb55bc4fe4b389090fb727cd611c56054107f2
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-16 13:40:32 +0530

    Apllied uniqueness validation

--- rails/my_first_project/app/models/user.rb
+++ rails/my_first_project/app/models/user.rb
@@ -3,4 +3,6 @@ class User < ApplicationRecord
   validates :first_name, presence: {message: "cant store blank"}	
   validates :mobileno, presence: true, length: { maximum: 10,
                        too_long: "%{count} characters is the maximum allowed"}
+  validates :mobileno, uniqueness: true                      
+
 end
