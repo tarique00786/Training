0a47a1927f77a55b952ee295732110d889e8d1b6
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-16 16:23:46 +0530

    Changes for using strict validation

--- rails/my_first_project/app/models/user.rb
+++ rails/my_first_project/app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
 
-  validates :first_name, presence: {message: "cant store blank"}	
+  validates :first_name, presence: {strict: true}
+    
   validates :mobileno, presence: true, length: { maximum: 10,
                        too_long: "%{count} characters is the maximum allowed"}
   
