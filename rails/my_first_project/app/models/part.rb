238eb3afb89c7aa040d0f392822f35868fe07668
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-16 18:15:34 +0530

    Did changes for using error bash

--- rails/my_first_project/app/models/part.rb
+++ rails/my_first_project/app/models/part.rb
@@ -1,7 +1,11 @@
 class Part < ApplicationRecord
   
   has_and_belongs_to_many :assemblies
+  #validate do |part|
+    #errors.add :part_number, :to_big, message: "Not valid"
+  #end
+  validates :part_number, presence: true
   validate do |part|
-    errors.add :part_number, :to_big, message: "Not valid"
-  end  	
+    errors.add :base, :invalid, message: "Invalid"
+  end    
 end
