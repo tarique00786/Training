c1579bcce93997aeb757cb877661d3e322303924
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-17 15:59:59 +0530

    Did changes to apply after_save callback

--- rails/my_first_project/app/models/task.rb
+++ rails/my_first_project/app/models/task.rb
@@ -1,8 +1,9 @@
 class Task < ApplicationRecord
   
   before_validation :set_title
-  before_save :change_title
+  #before_save :change_title
   #after_validation :set_title
+  after_save :change_title
   validates :title, presence:true, length: { maximum: 50}
   def set_title
     self.title = "Pay Electricity bill"
