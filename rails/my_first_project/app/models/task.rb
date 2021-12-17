a46ea2714f151f04feb6a40381d6b0e38f2d094c
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-17 15:30:30 +0530

    Did changes for using before_save callback

--- rails/my_first_project/app/models/task.rb
+++ rails/my_first_project/app/models/task.rb
@@ -1,9 +1,13 @@
 class Task < ApplicationRecord
-
-  #before_validation :set_title
-  after_validation :set_title
+  
+  before_validation :set_title
+  before_save :change_title
+  #after_validation :set_title
   validates :title, presence:true, length: { maximum: 50}
   def set_title
     self.title = "Pay Electricity bill"
+  end  
+  def change_title
+    self.title = "Pay Electricity and TV bill"
   end  	
 end
