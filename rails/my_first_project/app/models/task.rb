916db95b7e00d7cf0e7af3f30d6706a086e688f2
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-17 15:09:10 +0530

    Did changes in task model to perform after_validation callback

--- rails/my_first_project/app/models/task.rb
+++ rails/my_first_project/app/models/task.rb
@@ -1,6 +1,7 @@
 class Task < ApplicationRecord
 
-  before_validation :set_title
+  #before_validation :set_title
+  after_validation :set_title
   validates :title, presence:true, length: { maximum: 50}
   def set_title
     self.title = "Pay Electricity bill"
