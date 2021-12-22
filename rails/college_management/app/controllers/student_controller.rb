3a561e9ac31a85197a578c7a49055818499db21d
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-22 18:19:39 +0530

    Edited student controller to randering view

--- rails/college_management/app/controllers/student_controller.rb
+++ rails/college_management/app/controllers/student_controller.rb
@@ -3,6 +3,9 @@ class StudentController < ApplicationController
     @students = Student.all.order(name: :asc)  
   end
 
-  def delete
+  def delete_student
+    student = Student.find(params[:student_id])
+    student.destroy
+    redirect_to :action => 'index'
   end
 end
