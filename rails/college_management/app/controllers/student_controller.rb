60f0d9ec959eb5609efe3b320fc5cf9176433496
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-27 17:04:10 +0530

    Adds new and create methods in student controller

--- rails/college_management/app/controllers/student_controller.rb
+++ rails/college_management/app/controllers/student_controller.rb
@@ -1,7 +1,16 @@
 class StudentController < ApplicationController
+
   def index
     @students = Student.all.order(name: :asc)  
   end
+
+  def new
+
+  end 
+
+  def create
+
+  end 
 
   def delete_student
     student = Student.find(params[:student_id])
