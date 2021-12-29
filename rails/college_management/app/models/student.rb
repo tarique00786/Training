678cabef6e839c688fb152a245422f9f91433ffe
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-29 16:44:20 +0530

    Adds search method in student model

--- rails/college_management/app/models/student.rb
+++ rails/college_management/app/models/student.rb
@@ -1,5 +1,15 @@
 class Student < ApplicationRecord
   belongs_to :department
   validates :name, presence: true
-  validates :admission, numericality: {message: "Enter between 2018 and 2021"} 
+  validates :admission, numericality: {message: "Enter between 2018 and 2021"}
+  
+  def self.search(search)
+    
+    if search
+      where(["name LIKE ?","%#{search}%"])
+    else
+      all 
+    end   
+  end
+    
 end
