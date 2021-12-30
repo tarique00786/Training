d59f2cc146426bc5953565c938b56fd407891ab3
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-30 17:27:29 +0530

    Adds nested attributes for student

--- rails/college_management/app/models/department.rb
+++ rails/college_management/app/models/department.rb
@@ -2,4 +2,5 @@ class Department < ApplicationRecord
   has_many :students, dependent: :destroy
   has_many :department_staffs
   has_many :staffs, through: :department_staffs
+  accepts_nested_attributes_for :students, update_only: true
 end
