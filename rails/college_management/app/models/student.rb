461f19326f349b1a1bb7734acae3d5e2036c4613
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-30 12:22:03 +0530

    Adds pagination condition in search method

--- rails/college_management/app/models/student.rb
+++ rails/college_management/app/models/student.rb
@@ -3,9 +3,9 @@ class Student < ApplicationRecord
   validates :name, presence: true
   validates :admission, numericality: {message: "Enter between 2018 and 2021"}
   
-  def self.search(search)
+  def self.search(search, page_number)
     if search
-      where(["name ILIKE ?","%#{search}%"])
+      where(["name ILIKE ?","%#{search}%"]).paginate(:page => page_number, per_page: 20)
     else
       all 
     end   
