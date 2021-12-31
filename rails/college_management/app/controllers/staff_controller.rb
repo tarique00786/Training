75cab187dedb5a7f46e9d6590a7b94e5924e6a83
tarique00786 <tariqueaziz00786@gmail.com>
2021-12-31 13:07:13 +0530

    Adds instance variable of staff to show single staff details

--- rails/college_management/app/controllers/staff_controller.rb
+++ rails/college_management/app/controllers/staff_controller.rb
@@ -3,6 +3,10 @@ class StaffController < ApplicationController
     @staffs = Staff.all  
   end
 
+  def show
+    @staff = Staff.find(params[:id])
+  end 
+   
   def delete_staff
     staff = Staff.find(params[:staff_id])
     staff.destroy
