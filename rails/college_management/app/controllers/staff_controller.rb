6ef64cf3b1320c74da91ae01ef3d5178928598fa
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-03 11:30:49 +0530

    Adds new and create action in staff

--- rails/college_management/app/controllers/staff_controller.rb
+++ rails/college_management/app/controllers/staff_controller.rb
@@ -5,7 +5,23 @@ class StaffController < ApplicationController
 
   def show
     @staff = Staff.find(params[:id])
+  end
+
+  def new
+    @staff = Staff.new
+    @departments = Department.all
   end 
+
+  def create
+    @departments = Department.all
+    @staff = Staff.new(params.require(:staff).
+      permit(:id, :name, :salary, :address))
+    if @staff.save
+      redirect_to "/staffs/#{@staff.id}"
+    else
+      render 'new' 
+    end   
+  end  
   
   def edit
     @staff = Staff.find(params[:id])
