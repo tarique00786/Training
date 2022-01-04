bf72c32c561dc9c4ea8a65407cf9e8f4e72c809f
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-04 12:03:23 +0530

    Adds configuration for action mailer on 03 january 2022

--- rails/college_management/config/environments/development.rb
+++ rails/college_management/config/environments/development.rb
@@ -73,4 +73,16 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'testexamplecms123@gmail.com',
+  password:             'Rameez1@',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
+
 end
