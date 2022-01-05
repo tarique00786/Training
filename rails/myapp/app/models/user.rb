adee7fdc3de561cfab437e7889fbc4bb32d488d4
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-05 16:22:01 +0530

    Adds github auth in user model

--- rails/myapp/app/models/user.rb
+++ rails/myapp/app/models/user.rb
@@ -3,5 +3,14 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable,
-         :confirmable, :lockable, :trackable
+         :confirmable, :lockable, :trackable, :timeoutable, :omniauthable,
+         omniauth_providers: %i[github]
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.name = auth.info.name
+      user.password = devise.friendly_token[0,20]
+    end 
+  end        
 end
