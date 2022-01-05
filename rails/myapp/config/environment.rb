5f3114815095a71c06d6b70e095fbeaac33d6c30
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-05 06:53:35 +0530

    Adds the configuration for mailer

--- rails/myapp/config/environment.rb
+++ rails/myapp/config/environment.rb
@@ -3,3 +3,15 @@ require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.perform_deliveries = true
+  ActionMailer::Base.smtp_settings = {
+  address:              'smtp.gmail.com',
+  port:                 587,
+  domain:               'gmail.com',
+  user_name:            'testexamplecms123@gmail.com',
+  password:             'Rameez1@',
+  authentication:       'plain',
+  enable_starttls_auto: true,
+  open_timeout:         5,
+  read_timeout:         5 }
