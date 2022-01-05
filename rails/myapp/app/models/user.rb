80836af014ef21293d14ae3681a6a41e2c8d93a2
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-05 17:01:49 +0530

    Adds new self function for applying omniauth

--- rails/myapp/app/models/user.rb
+++ rails/myapp/app/models/user.rb
@@ -6,11 +6,15 @@ class User < ApplicationRecord
          :confirmable, :lockable, :trackable, :timeoutable, :omniauthable,
          omniauth_providers: %i[github]
 
-  def self.from_omniauth(auth)
-    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
-      user.email = auth.info.email
-      user.name = auth.info.name
-      user.password = devise.friendly_token[0,20]
-    end 
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+    unless user
+      user = User.create(
+          email: data['email'],
+          password: Devise.friendly_token[0,20]
+       )
+    end
+    user
   end        
 end
