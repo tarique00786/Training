ee8d3e0e8c7ef6e40e1470b5ba8d5c37bf77e26e
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-05 18:05:08 +0530

    Changes happens because of schema

--- rails/myapp/db/schema.rb
+++ rails/myapp/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_05_052803) do
+ActiveRecord::Schema.define(version: 2022_01_05_064537) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,6 +35,8 @@ ActiveRecord::Schema.define(version: 2022_01_05_052803) do
     t.datetime "last_sign_in_at"
     t.inet "current_sign_in_ip"
     t.inet "last_sign_in_ip"
+    t.string "provider"
+    t.string "uid"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
     t.index ["unlock_token"], name: "index_users_on_unlock_token", unique: true
