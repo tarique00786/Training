94bae316d7ff6b44b2f4cc86d4b11e4ab210dadd
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-06 12:19:21 +0530

    Adds has_one_attached to student model

--- rails/college_management/app/models/student.rb
+++ rails/college_management/app/models/student.rb
@@ -2,7 +2,7 @@ class Student < ApplicationRecord
   belongs_to :department, optional: true
   validates :name, presence: true
   validates :admission, numericality: {message: "Enter between 2018 and 2021"}
-  
+  has_one_attached :image
   def self.search(search, page_number)
     if search
       where(["name ILIKE ?","%#{search}%"]).paginate(:page => page_number, per_page: 20)
