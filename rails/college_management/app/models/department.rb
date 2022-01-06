f281482ce4a7738939b9c6b9cffbc374234a1c5d
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-06 17:21:16 +0530

    Adds has_many_attached in department

--- rails/college_management/app/models/department.rb
+++ rails/college_management/app/models/department.rb
@@ -3,4 +3,5 @@ class Department < ApplicationRecord
   has_many :department_staffs
   has_many :staffs, through: :department_staffs
   accepts_nested_attributes_for :students, update_only: true
+  has_many_attached :images
 end
