ce60b22b4a62ff46255c6e830c1d524dd8955aab
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-07 10:54:25 +0530

    Adds code for the avatar Uploader

--- rails/college_management/app/models/student.rb
+++ rails/college_management/app/models/student.rb
@@ -1,9 +1,10 @@
 class Student < ApplicationRecord
+  mount_uploader :avatar, AvatarUploader
   belongs_to :department, optional: true
   validates :name, presence: true
   validates :admission, numericality: {message: "Enter between 2018 and 2021"}
   has_one_attached :image
-  validates :image, attached: true, content_type: 'image/jpeg', size: {greater_than: 100.kilobytes, message: 'Not between size'}
+  validates :image, attached: true, content_type: 'image/jpeg', size: {less_than: 100.kilobytes, message: 'Not between size'}
   def self.search(search, page_number)
     if search
       where(["name ILIKE ?","%#{search}%"]).paginate(:page => page_number, per_page: 20)
