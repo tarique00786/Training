33d5792d9e2961140d9d8020f75d90e672505c7d
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-07 13:19:57 +0530

    import direct upload in application.js

--- rails/college_management/app/javascript/packs/application.js
+++ rails/college_management/app/javascript/packs/application.js
@@ -7,7 +7,8 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-
+import { DirectUpload } from "@rails/activestorage"
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+import "direct_uploads"
