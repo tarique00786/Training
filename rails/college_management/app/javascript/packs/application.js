80d78e682847f45631c54510f63bb4603d73a42a
tarique00786 <tariqueaziz00786@gmail.com>
2022-01-08 15:40:56 +0530

    adds code to create progress bar

--- rails/college_management/app/javascript/packs/application.js
+++ rails/college_management/app/javascript/packs/application.js
@@ -2,7 +2,11 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-
+require activestorage
+require jquery
+require refile
+require_tree
+require jquery-fileupload/basic
 import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
@@ -12,3 +16,22 @@ Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
 import "direct_uploads"
+
+$(document).ready(function() {
+ var uploadObj = $("#staff_clips").uploadFile({
+    url: "/staffs",
+    multiple: false,
+    fileName: "clips",
+    autoSubmit: false,
+    onSuccess:function(files,data,xhr)
+    {
+      window.location.href = data.to;
+    }
+  });
+
+  $("#fileUpload").click(function(e) {
+    e.preventDefault();
+    $.rails.disableFormElements($($.rails.formSubmitSelector));
+    uploadObj.startUpload();
+  });
+});
